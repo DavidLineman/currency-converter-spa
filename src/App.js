031a3192eb13feedda971f9d3bd2a1de85c4a3db
DavidLineman <dline215@gmail.com>
2022-03-04 09:39:14 -0500

    removed some dependencies

--- src/App.js
+++ src/App.js
@@ -3,22 +3,35 @@ import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">FastRates!</a>
+        </div>
+      </nav>
     </div>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-12 col-md-6'>
+          <label>Amount</label>
+          <input type="number"></input>
+            <div className="btn-group">
+              <button type="button" className="btn btn-success dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                Action
+              </button>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                <li><a className="dropdown-item" href="#">Separated link</a></li>
+              </ul>
+            </div>
+        </div>
+        
+      </div>
+    </div>
+    </>
   );
 }
 
